37c2c3172896a7c52b885c3eaf406c91805d6aec
nfxbeats <nfxbeats@gmail.com>
2022-07-19 12:50:35 -0700

    formatting, extra documentation

--- fireNFX_DEFAULTS.py
+++ fireNFX_DEFAULTS.py
@@ -1,16 +1,28 @@
+# DO NOT CHANGE THE FOLLOWING IMPORT LINES
 from fireNFX_Colors import *
 from fireNFX_Defs import NotesListFlats, NotesListSharps
 
-
-#   valid colors are: cWhite, cBlue, cGreen, cRed, cYellow, cCyan, cPurple, cOrange, cMagenta and cOff 
 #
+#   Rules:
+#
+#   hastag lines # are comments. READ THEM to understand the values you need to provide for the setting below it.
+#
+#   lines with a DEFAULT_XXXX value can be edited after the = symbol to set that value
+#
+#   True or False values must be spelled exactly as "True" or "False". using true/TRUE or false/FALSE will not work.
+#
+#   valid colors are: 
+#       cWhite, cBlue, cGreen, cRed, cYellow, cCyan, cPurple, cOrange, cMagenta and cOff 
+#
+#
+#____________ OK TO EDIT AFTER THIS LINE______________
 
 # color of pads when pressed.
-DEFAULT_PAD_PRESSED_COLOR = cYellow
+DEFAULT_PAD_PRESSED_COLOR = cRed
 
 # color of pads when pressed.
 # use  cChannel to use the channel color
-DEFAULT_ROOT_NOTE_COLOR = cBlue 
+DEFAULT_ROOT_NOTE_COLOR = cChannel
 
 # this is an interal way to control the brightness for things like vs non active pads
 # value can be 0-5 where 0 is NO DIMMING and 5 is MAX DIMMING
@@ -23,7 +35,7 @@ DEFAULT_DIM_BRIGHT = 1
 DEFAULT_NOTE_NAMES = NotesListSharps
 
 # Default Root Note. If you select NotesListSharps in the DEFAULT_NOTE_NAMES, use '#' for non natural notes. 
-# otherwise use 'b' for the the flat name. ie C# vs. Db
+# otherwise use 'b' for the the flat name. ie 'C#' for NotesListSharps or 'Db' for NotesListFlats
 DEFAULT_ROOT_NOTE = 'C'                      
 
 # lowest octave (bottom row) in NOTE mode. 
@@ -38,21 +50,25 @@ DEFAULT_OCTAVE = 3
 #   8 = Major Pentatonic    9 = Minor Pentatonic
 DEFAULT_SCALE = 0
 
-DEFAULT_SHOW_ALL_MATCHING_CHORD_NOTES = False # when True, it will show all pads that have the same note mappings.
-
+# when True, it will show all pads that have the same/repeated note mappings when octave notes overflow
+DEFAULT_SHOW_ALL_MATCHING_CHORD_NOTES = False 
 
 # Whether or not to light the channel strip will indicate
 # other channels that share the same mixer routing
-#
-DEFAULT_SHOW_CHANNEL_WITH_SHARED_MIXER_CHANNELS = True # will display the channels that have a common mixer number with the selected channel
+DEFAULT_SHOW_CHANNEL_WITH_SHARED_MIXER_CHANNELS = True 
 
-DEFAULT_SHOW_PLAYBACK_NOTES = True # in NOTE mode, this will show what note is playing on playback (limited to mono - ie one note at a time)
+# in NOTE mode, this will show what note is playing on playback (limited to mono - ie one note at a time)
+DEFAULT_SHOW_PLAYBACK_NOTES = True 
 
-DEFAULT_REOPEN_WINDOWS_AFTER_CLOSE_ALL = True # after a close all you have the core windows re-open automatically
+# after a close all you have the core windows re-open automatically
+DEFAULT_REOPEN_WINDOWS_AFTER_CLOSE_ALL = True 
 
-DEFAULT_MARKER_PREFIX_TEXT = "nfx#"  # markers created by the script will have this prefix 
+# markers created by the script will have this prefix 
+DEFAULT_MARKER_PREFIX_TEXT = "nfx#"  
 
-DEFAULT_SHOW_PRN = False # shows extra info I use when debugging. may affect performance when True
+# shows extra info I use when debugging. may affect performance when True
+DEFAULT_SHOW_PRN = False 
 
-DEFAULT_ALT_DRUM_MODE_BANKS = True # when true, will display four banks in Al DRUM mode. otherwise it uses Row4..Row1 order
+# when true, will display four banks in ALT+DRUM mode. otherwise it uses octave strips in Row4..Row1 order
+DEFAULT_ALT_DRUM_MODE_BANKS = True 
 
